43a89cd1bc2abc1bd4df97be15f79380a522337c
leerivera <leerivera80@gmail.com>
2021-01-14 07:05:08 -0500

    basic op

---
+++ katas/basicop.js
@@ -0,0 +1,11 @@
+//   Your task is to create a function that does four basic mathematical operations.
+
+//   The function should take three arguments - operation(string/char), value1(number), value2(number).
+//   The function should return result of numbers after applying the chosen operation.
+
+
+
+function basicOp(operation, value1, value2)
+{
+  return eval(value1 + operation + value2);
+}
