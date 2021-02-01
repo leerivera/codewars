520dc5ba007c7375128c0c3d71b9318dceedbcec
leerivera <leerivera80@gmail.com>
2021-02-01 09:08:07 -0500

    soulution 2

--- katas/setalarm.js
+++ katas/setalarm.js
@@ -1,3 +1,7 @@
 //8kyu l1 set alarm
 
 const setAlarm = (employed, vacation) => employed && !vacation;
+
+function setAlarm(employed, vacation){
+    return (employed && !vacation) ? true : false;
+  }
