156cf932212cbf8133199490fb066ad61c607876
leerivera <leerivera80@gmail.com>
2021-02-06 07:09:45 -0500

    dbl char

---
+++ katas/dblchar.js
@@ -0,0 +1,3 @@
+// 8kyu double char
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+let doubleChar =(str) => str.split('').map(x=> x+x).join('')
--- katas/reversestr.js
+++ katas/reversestr.js
@@ -3,3 +3,11 @@
 function solution(str){
     str.split('').reverse('').join('')
   }
+
+
+function solution(s){
+    var o = '';
+    for (var i = s.length - 1; i >= 0; i--)
+      o += s[i];
+    return o;
+  }
