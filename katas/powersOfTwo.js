6a68610ba45e963851a2e0ebe21b7df16b010d9e
leerivera <leerivera80@gmail.com>
2021-06-22 11:18:29 -0400

    solution4

--- katas/powersOfTwo.js
+++ katas/powersOfTwo.js
@@ -30,3 +30,5 @@ function powersOfTwo(n){
   function powersOfTwo(n) {
     return [...Array(n + 1)].map((_, i) => 2 ** i)
   }
+
+  powersOfTwo = n => [...Array(n + 1)].map((e, i) => Math.pow(2, i));
