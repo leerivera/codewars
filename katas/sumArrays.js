3e81c39e2581f683efc4d057e63c3a41b8286c8f
leerivera <leerivera80@gmail.com>
2021-06-24 01:18:10 -0400

    sum arrays?

---
+++ katas/sumArrays.js
@@ -0,0 +1,4 @@
+// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+
+sum = numbers => numbers.reduce((prev, curr) => prev + curr, 0) 
