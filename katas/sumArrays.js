ab870fd94eaf7ebe41c9f31514e7a9f98f9e4ddd
leerivera <leerivera80@gmail.com>
2021-06-26 01:18:10 -0400

    solution3

--- katas/sumArrays.js
+++ katas/sumArrays.js
@@ -11,3 +11,16 @@ sum = function (numbers) {
     }
     return total;
   };
+
+  function sum (numbers) {
+    if(numbers.length === 0){
+      return 0;
+    }
+    else {
+      let sumNumbers = 0;
+      for (e of numbers) {
+        sumNumbers += e;
+      }
+      return sumNumbers;
+    }    
+  };
