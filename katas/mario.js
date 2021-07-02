3756da0b144caa61b3c509f7ac40db9b3d5aac27
leerivera <leerivera80@gmail.com>
2021-07-02 03:22:25 -0400

    solution2

--- katas/mario.js
+++ katas/mario.js
@@ -16,3 +16,14 @@
 function pipeFix(numbers){
     return Array.from({length: numbers.slice(-1) - numbers[0] +1}, (_, index) => index + numbers[0])
 }
+
+function pipeFix(numbers){
+    var first = numbers[0];
+    var last = numbers[numbers.length-1];
+    
+    var arr = [];
+    for(var i = first; i <= last; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }
