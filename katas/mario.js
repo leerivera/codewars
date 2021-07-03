30daff24122a818e24a516637ea274ec9fb2c66a
leerivera <leerivera80@gmail.com>
2021-07-03 02:25:00 -0400

    solution3

--- katas/mario.js
+++ katas/mario.js
@@ -27,3 +27,11 @@ function pipeFix(numbers){
     }
     return arr;
   }
+
+  function pipeFix(numbers){
+    var n_array = [];
+    for(var i=numbers[0];i<=numbers[numbers.length-1];i++){
+      n_array.push(i);
+    }
+    return n_array;
+  }
