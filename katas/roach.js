f6d8a5bb0e10eb33048951ba797141da30a6cfa1
leerivera <leerivera80@gmail.com>
2021-07-21 09:40:25 -0400

    solution2

--- katas/roach.js
+++ katas/roach.js
@@ -3,3 +3,10 @@
 function cockroachSpeed(s) {
     return Math.floor(s * 27.7778);
   }
+
+  function cockroachSpeed(s) {
+    const secsInHour = 3600;
+    const centimetersInKilometers = 100000;
+    
+    return Math.floor((s*centimetersInKilometers)/secsInHour);
+  }
