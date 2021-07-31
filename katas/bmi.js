c218605065b196e01604d1cb9045ba4d13c99c6b
leerivera <leerivera80@gmail.com>
2021-07-31 11:07:16 -0400

    solution2

--- katas/bmi.js
+++ katas/bmi.js
@@ -27,3 +27,18 @@ function bmi(weight, height){
     }
     
   }
+
+  function bmi(weight, height) {
+    var formula = (weight / Math.pow(height, 2));
+    switch (true) {
+      case formula <=18.5:
+      return 'Underweight';
+      case formula <=25.0:
+      return 'Normal';
+      case formula <=30:
+      return 'Overweight';
+      default:
+      return 'Obese';
+      
+    }
+  }
