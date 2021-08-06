65890d43e0ca6b7c20270e0c790a76860cccada9
leerivera <leerivera80@gmail.com>
2021-08-06 09:01:57 -0400

    solution3

--- katas/grasshopperSummation.js
+++ katas/grasshopperSummation.js
@@ -11,3 +11,11 @@ var summation = function (num) {
 }
 
 const summation = n => n * (n + 1) / 2;
+
+var summation = function (num) {
+    var sum = 0;
+    for(var i=0; i<=num;i++){
+      sum+=i
+    }
+    return sum;
+  }
