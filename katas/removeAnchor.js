f9f64bd976ae615649675c678962e2686b74a3ea
leerivera <leerivera80@gmail.com>
2021-08-19 01:09:37 -0400

    solution4

--- katas/removeAnchor.js
+++ katas/removeAnchor.js
@@ -4,6 +4,10 @@
 // "www.codewars.com#about" --> "www.codewars.com"
 // "www.codewars.com?page=1" -->"www.codewars.com?page=1"
 
+function removeUrlAnchor(url) {
+    var index = url.indexOf("#");
+    return index == -1 ? url : url.substring(0, url.indexOf("#"));
+  }
 
 
 function removeUrlAnchor(url){
