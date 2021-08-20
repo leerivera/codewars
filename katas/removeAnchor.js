9bdb3b46cf32f4848bea4c7fcc787f9d120e435f
leerivera <leerivera80@gmail.com>
2021-08-20 02:09:45 -0400

    solution3

--- katas/removeAnchor.js
+++ katas/removeAnchor.js
@@ -6,7 +6,14 @@
 
 
 
-
+function removeUrlAnchor(url){
+    let string = ""
+    for(let i = 0; i < url.length; i++){
+      if(url[i] === "#") { break }
+      string += url[i]
+    }
+    return string 
+  }
 
 
 
