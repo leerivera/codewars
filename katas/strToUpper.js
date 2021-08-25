612cfd6b6e3732b9e2fe403d2f658f6b833bd9ee
leerivera <leerivera80@gmail.com>
2021-08-25 12:08:17 -0400

    str to upper

---
+++ katas/strToUpper.js
@@ -0,0 +1,9 @@
+function DNAtoRNA(dna) {
+    // create a function which returns an RNA sequence from the given DNA sequence
+    for (var i = 0; i < dna.length; i++)
+    {
+      dna = dna.replace('T', 'U');
+    }
+    
+    return dna;
+  }
