88401d39aba06dfd4a36b223f184a6045d8bf5dc
leerivera <leerivera80@gmail.com>
2021-08-26 01:10:06 -0400

    solution3

--- katas/DnaToRNA.js
+++ katas/DnaToRNA.js
@@ -30,3 +30,15 @@ function DNAtoRNA(dna) {
   function DNAtoRNA(dna){
     return dna.replace(/T/g, 'U');
   }
+
+  function DNAtoRNA(dna) {
+    var hold = ''
+    for(var i =0;i<dna.length;i++) {
+       if(dna[i]=="T") {
+          hold+="U"
+  }
+  else{hold+=dna[i]}
+  }
+  return hold;
+        
+  }
