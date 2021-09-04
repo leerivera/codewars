eaa7889765a4ba0b830879d8a82b475a041fe156
leerivera <leerivera80@gmail.com>
2021-09-04 10:09:25 -0400

    solution4

--- katas/repeatstring.js
+++ katas/repeatstring.js
@@ -20,3 +20,11 @@ function repeatStr (n, s) {
     }
     return newString
 }
+
+function repeatStr (n, s) {
+    let newString = []
+    for(let i = 0; i < n; i++) {
+        newString.push(s)
+    }
+    return newString.join('')
+}
