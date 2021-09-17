0237c7a3194b44f17c44944459322cb45b7913d5
leerivera <leerivera80@gmail.com>
2021-09-17 05:08:45 -0400

    make negative

---
+++ katas/makeNegative.js
@@ -0,0 +1,12 @@
+// return negative 8kyu
+
+//   In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Example:
+
+// makeNegative(1); // return -1 makeNegative(-5); // return -5 makeNegative(0); // return 0
+
+function makeNegative(num) {
+    return (num > 0) ? parseInt(`-${num}`, 10) : num
+  
+  }
