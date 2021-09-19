b302f45c4093fe57f9980b211b5c201b539d9c73
leerivera <leerivera80@gmail.com>
2021-09-19 08:10:50 -0400

    solution2

--- katas/lostwWithOutMap.js
+++ katas/lostwWithOutMap.js
@@ -3,3 +3,13 @@
 function maps(x){
     return x.map(el => el *2)
   }
+
+
+function maps(x) {
+  //return x.map(el => el * 2);
+  let arr = [];
+  for (let i = 0; i < x.length; i++) {
+    arr.push(x[i] * 2);
+  }
+  return arr;
+}
