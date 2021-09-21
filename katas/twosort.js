7f41fa1a792d53f6b7c952c91421da864b521ad1
leerivera <leerivera80@gmail.com>
2021-09-21 12:33:48 -0400

    solution2

--- katas/twosort.js
+++ katas/twosort.js
@@ -11,3 +11,7 @@ twoSort = (s) => s.sort()[0].split('').join('***')
 function twoSort(array) {
     return [...array.reduce((min, str) => min < str ? min : str)].join("***");
   }
+
+  function twoSort(array) {
+    return [...array.reduce((min, str) => min < str ? min : str)].join("***");
+  }
