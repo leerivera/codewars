a31b1a9081394d4f950bc5873fc13d17c96c1567
leerivera <leerivera80@gmail.com>
2021-09-29 12:27:49 -0400

    solution3

--- katas/feast.js
+++ katas/feast.js
@@ -14,3 +14,8 @@ function feast(beast, dish) {
     function feast(beast, dish) {
         return beast[0]===dish[0] && beast.slice(-1)===dish.slice(-1);
       }
+
+      function feast(beast, dish) {
+        return dish.startsWith(beast[0]) && dish.endsWith(beast[beast.length-1])
+      }
+      
