9ad6eceb582bb0d10c6259c8ab0e562838aa6a70
leerivera <leerivera80@gmail.com>
2021-09-30 12:27:49 -0400

    solution2

--- katas/feast.js
+++ katas/feast.js
@@ -10,3 +10,7 @@ function feast(beast, dish) {
     //your function here
     return beast[0] === dish[0] && beast[beast.length-1] === dish[dish.length-1]
     }
+
+    function feast(beast, dish) {
+        return beast[0]===dish[0] && beast.slice(-1)===dish.slice(-1);
+      }
