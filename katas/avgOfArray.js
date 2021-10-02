da238e68fc5f315fb1c41c166cd3c4a6a626ad7e
leerivera <leerivera80@gmail.com>
2021-10-02 12:23:48 -0400

    solution2

--- katas/avgOfArray.js
+++ katas/avgOfArray.js
@@ -1,3 +1,14 @@
 // calculate avg of an array
 
 let find_average = (array) => array.length > 0 ? array.reduce((acc,c)=> acc + c, 0)/array.length : 0
+
+function find_average(array) {
+    if (array.length === 0) {
+    return 0;
+    }
+    var result = 0;
+    for (i=0; i<array.length; i++) {
+      result +=array[i];
+    }
+    return result/array.length;
+  }
