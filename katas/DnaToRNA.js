7e1d40d02adc2fce0b6be4add96ff769c8285676
leerivera <leerivera80@gmail.com>
2022-06-02 19:14:40 -0400

    solution5

--- katas/DnaToRNA.js
+++ katas/DnaToRNA.js
@@ -52,3 +52,13 @@ function DNAtoRNA(dna) {
     
     return dna;
   }
+
+  function DNAtoRNA(dna) {
+    // create a function which returns an RNA sequence from the given DNA sequence
+    for (var i = 0; i < dna.length; i++)
+    {
+      dna = dna.replace('T', 'U');
+    }
+    
+    return dna;
+  }
